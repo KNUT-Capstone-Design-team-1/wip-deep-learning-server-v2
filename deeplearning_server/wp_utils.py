39ae453207385c51a41de12d91e83d81a744d7fb
YongSihun <tlgns1537@naver.com>
2023-01-18 21:34:10 +0900

    230118 변수명 변경

--- deeplearning_server/wp_utils.py
+++ deeplearning_server/wp_utils.py
@@ -11,7 +11,7 @@ def make_json(pill_shape, pill_text, pill_line=False):
         "is_success": True,
         "data": [{
             "print": "",
-            "chartin": "",
+            "chartn": "",
             "drug_shape": "",
             "color_class": "",
             "line_front": "",

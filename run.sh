c4017238eb8573e9fe14948bd359b334e95bcf89
YongSihun <tlgns1537@naver.com>
2023-08-25 21:32:07 +0900

    #3 - 딥러닝 서버 실행 스크립트 수정
    - 컨테이너 실행되지 않는 문제 수정

--- run.sh
+++ run.sh
@@ -1,33 +1,34 @@
-!#/bin/bash
+#!/bin/bash
 
-# set environment values
+# 서버 내 .env 파일로 부터 환경변수 정의
+echo "---- set environment values ----"
 unamestr=$(uname)
 
-if [ "$unamestr" = 'Linux' ]; then
+if [ "$unamestr"='Linux' ]; then
   export $(grep -v '^#' .env | xargs -d '\n')
 
-elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
+elif [ "$unamestr"='FreeBSD' ] || [ "$unamestr"='Darwin' ]; then
   export $(grep -v '^#' .env | xargs -0)
 fi
+echo "---- OK ----"
 
-# container stop
-docker stop wip-dl
-
-# docker image delete
-build_mode=$1
-if [$build_mode = "rebuild"]; then
-  docker rmi wip-dl
-fi
-
-# docker image build
-build_cmd="docker build -t wip-dl"
+# 도커 이미지 빌드
+echo "---- container image build ----"
+build_cmd="docker build . -t wip-dl"
 while read line; do
    arg_temp=$(echo $line | cut -f 1 -d'=')
    build_cmd+=" --build-arg $arg_temp=$(eval echo '$'$arg_temp)"
 done < .env
 
-build_cmd+=" ."
-echo $build_cmd
+$(echo $build_cmd)
+echo "---- OK ----"
 
-# docker container run
-docker run wip-dl -d
+# 컨테이너 종료. 실행중인 컨테이너가 있으면 강제로 제거
+echo "---- remove previous container ----"
+docker container rm -f wip-dl
+echo "---- OK ----"
+
+# 컨테이너 실행
+echo "---- run container ----"
+docker run -d --name wip-dl wip-dl
+echo "---- OK ----"

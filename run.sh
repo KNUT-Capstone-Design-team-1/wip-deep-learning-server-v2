60ddd6de9da2dd2ae67ba549a8abfe374b698202
YongSihun <tlgns1537@naver.com>
2024-03-18 23:54:50 +0900

    #7 run.sh에서 매번 실행될 필요 없는 항목 제거

--- run.sh
+++ run.sh
@@ -15,14 +15,10 @@ echo "---- OK ----"
 echo "---- $1 ----"
 
 if [ $1 = "STAND-ALONE" ]; then
-  # 모든 python 종료. 차후에는 systemd로 개선 필요
-  killall python
-  apt-get update && apt-get -y install libgl1-mesa-glx libapache2-mod-wsgi \
-      && /usr/local/bin/python -m pip install --upgrade pip \
-      && pip3 install compiler \
-      && pip3 install -r /usr/local/wip-deep-learning-server-v2/requirements.txt \
-      && python3 /usr/local/wip-deep-learning-server-v2/get_dl_model.py
-  python3 /must_be_madness/what_is_pill/wip-deep-learning-server-v2/DL_main.py
+  sudo systemctl stop wip-deep-learning-server-v2
+  sudo pip3 install -r /usr/local/wip-deep-learning-server-v2/requirements.txt
+  sudo python3 /usr/local/wip-deep-learning-server-v2/get_dl_model.py
+  sudo systemctl start wip-deep-learning-server-v2
 elif [ $1 = "SINGLE-CONTAINER" ]; then
   # 도커 이미지 빌드
   echo "---- container image build ----"

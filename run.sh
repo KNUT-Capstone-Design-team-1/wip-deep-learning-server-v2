2e7b110902cbc0d09f991a8df3aa7c1385d84683
YongSihun <tlgns1537@naver.com>
2024-03-19 21:12:35 +0900

    #7 Docker 이미지 빌드 시 캐시 없이 빌드하도록 수정
    - 로깅개선

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-# 서버 내 .env 파일로 부터 환경변수 정의
-echo "---- set environment values ----"
+echo "---- Set environment values ----"
 unamestr=$(uname)
 
 if [ $unamestr = "Linux" ]; then
@@ -15,14 +13,22 @@ echo "---- OK ----"
 echo "---- $1 ----"
 
 if [ $1 = "STAND-ALONE" ]; then
+  echo "---- Stop wip-deep-learning-server-v2 ----"
   sudo systemctl stop wip-deep-learning-server-v2
+  echo "---- OK ----"
+
+  echo "---- Update require library ----"
   sudo pip3 install --upgrade --ignore-installed -r /must_be_madness/what_is_pill/wip-deep-learning-server-v2/requirements.txt
   sudo python3 /must_be_madness/what_is_pill/wip-deep-learning-server-v2/get_dl_model.py
+  echo "---- OK ----"
+
+  echo "---- Start wip-deep-learning-server-v2 ----"
   sudo systemctl start wip-deep-learning-server-v2
+  echo "---- OK ----"
 elif [ $1 = "SINGLE-CONTAINER" ]; then
-  # 도커 이미지 빌드
-  echo "---- container image build ----"
-  build_cmd="docker build . -t wip-deep-learning-server-v2"
+  echo "---- Build Container image ----"
+  build_cmd="docker build . --no-cache -t wip-deep-learning-server-v2"
+
   while read line; do
     arg_temp=$(echo $line | cut -f 1 -d'=')
     build_cmd+=" --build-arg $arg_temp=$(eval echo '$'$arg_temp)"
@@ -31,13 +37,11 @@ elif [ $1 = "SINGLE-CONTAINER" ]; then
   $(echo $build_cmd)
   echo "---- OK ----"
 
-  # 컨테이너 종료. 실행중인 컨테이너가 있으면 강제로 제거
-  echo "---- remove previous container ----"
+  echo "---- Remove previous container ----"
   docker container rm -f wip-deep-learning-server-v2
   echo "---- OK ----"
 
-  # 컨테이너 실행
-  echo "---- run container ----"
+  echo "---- Run container ----"
   docker run -d --name wip-deep-learning-server-v2 wip-deep-learning-server-v2
   echo "---- OK ----"
 fi
